3b5e2700b489aab0d23d92e0a8afad84768aaa03
amir <amirtaki623@gmail.com>
2025-07-04 23:06:46 +0330

    option js at project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const select = document.querySelector(".select")
 const options_list = document.querySelector(".options-list")
-const options = document.querySelectorAll(".options")
+const options = document.querySelectorAll(".option")
 
 
 // show & hide options list
@@ -8,4 +8,16 @@ const options = document.querySelectorAll(".options")
 select.addEventListener("click", (e)=>{
     options_list.classList.toggle("active")
     select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up")
+})
+
+
+// select option 
+options.forEach((option) => {
+    option.addEventListener("click", ()=> {
+        options.forEach((option) => {
+            option.classList.remove('selected')
+        })
+        select.querySelector("span").innerHTML = option.innerHTML;
+    })
+
 })
