b8dc7c4afecdf75fd499c14b762d0f5e011d28ff
amir <amirtaki623@gmail.com>
2025-07-04 22:58:01 +0330

    addEvent lister at project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 const select = document.querySelector(".select")
 const options_list = document.querySelector(".options-list")
 const options = document.querySelectorAll(".options")
+
+
+// show & hide options list
+
+select.addEventListener("click", (e)=>{
+    options_list.classList.toggle("active")
+})
